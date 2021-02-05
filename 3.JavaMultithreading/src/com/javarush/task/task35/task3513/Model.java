855d4cf4862bf49c284f6b582f944b1cc5921c1b
marina <marinagavrilova2012@gmail.com>
2021-02-05 14:34:40 +0300

    model class added

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,43 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        resetGameTiles ();
+    }
+
+    public void resetGameTiles() {
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                gameTiles[i][j] = new Tile (  );
+            }
+        }
+        addTile ();
+        addTile ();
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> list = new ArrayList<> (  );
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                if (gameTiles[i][j].isEmpty ()) {
+                    list.add ( gameTiles[i][j] );
+                }
+            }
+        }
+        return list;
+    }
+
+    private void addTile() {
+        List<Tile> list = getEmptyTiles ();
+        if (list.size () < 1) return;
+        int randomId = (int) (list.size () * Math.random ());
+        list.get ( randomId ).value = Math.random() < 0.9 ? 2 : 4;
+    }
 }
